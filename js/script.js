62cbaa2e42033ebe39148832ceeb90a2bd1b8eac
Fedekh <federicocet@gmail.com>
2023-03-29 16:34:55 +0200

    add BONUS 1 COMPLETE

--- js/script.js
+++ js/script.js
@@ -14,31 +14,35 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      activeImage: 0,
-      time: 1500,
-      interval: "",
+      activeImage: 0,   //setto di defaul questo mio contatore per compararlo con gli indici all interno del ciclo for x add o no active class
+      time: 1500,       // per ora lo setto a 1,5 s il timer cosi da vedere bene se funge tutto
+      interval: "",     //mi servirà settato vuoto per far partire l'autoplay al refresh di pagina
       slides: [
         {
           image: 'img/01.webp',
           title: 'Marvel\'s Spiderman Miles Morale',
           text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
           alt: 'Spiderman volante'
-        }, {
+        },
+        {
           image: 'img/02.webp',
           title: 'Ratchet & Clank: Rift Apart',
           text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
           alt: 'Il topo e clank'
-        }, {
+        },
+        {
           image: 'img/03.webp',
           title: 'Fortnite',
           text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
           alt: 'Fortnite'
-        }, {
+        },
+        {
           image: 'img/04.webp',
           title: 'Stray',
           text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
           alt: 'Stray'
-        }, {
+        },
+        {
           image: 'img/05.webp',
           title: "Marvel's Avengers",
           text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
@@ -59,8 +63,9 @@ createApp({
       if (this.activeImage > 0) {
         this.activeImage--;
       } else {
-        this.activeImage = this.slides.length  - 1;
+        this.activeImage = this.slides.length - 1;
       }
+    }
   }
-}
 }).mount('#app')
+
